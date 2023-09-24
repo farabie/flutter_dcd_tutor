1aeb6f4c5c3ce86e354cb3fae821dac399cb23b6
Muhammad Farabie <farabie024@gmail.com>
2023-09-24 10:12:37 +0700

    ListViewBuilder & ListView Biasa

--- lib/ui/page/listview_flut.dart
+++ lib/ui/page/listview_flut.dart
@@ -5,8 +5,16 @@ class ListViewFlut extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView(
+    return Scaffold(body: ListViewF());
+  }
+}
+
+class ListViewF extends StatelessWidget {
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
       children: numberList.map((number) {
         return Container(
           height: 150,
@@ -20,6 +28,32 @@ class ListViewFlut extends StatelessWidget {
           ),
         );
       }).toList(),
-    ));
+    );
+  }
+}
+
+class ListViewB extends StatelessWidget {
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: numberList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 150,
+          decoration: BoxDecoration(
+            color: Colors.grey[400],
+            border: Border.all(color: Colors.black),
+          ),
+          child: Center(
+            child: Text(
+              '${numberList[index]}',
+              style: TextStyle(fontSize: 50),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
